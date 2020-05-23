68520da844f5aee35f93626e364d43fec7771656
i.bettini <i.bettini@campus.unimib.it>
2020-05-23 16:01:03 +0200

    update analisi clustal omega

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ for f in glob.glob('Results/Kalign/*.fasta'):
             if aln[i].seq[j].upper() != aln[0].seq[j].upper():
                 print("  position : " + str(j) + " reference : " + aln[0].seq[j] + " sequence : " + aln[i].seq[j], file=open("kalign-output.txt", "a")) 
 
-"""
+
 for f in glob.glob('Results/Clustal Omega/*.fasta'):
     aln = list(AlignIO.read(open(f), "fasta"))
     for i in range(1, len(aln)):
@@ -21,9 +21,9 @@ for f in glob.glob('Results/Clustal Omega/*.fasta'):
         for j in range(len(aln[i].seq)):
             if aln[i].seq[j].upper() != aln[0].seq[j].upper():
                 print("  position : " + str(j) + " reference base: " + aln[0].seq[j] + " sequence base: " + aln[i].seq[j], file=open("clustalomega-output.txt", "a")) 
-"""
 
-"""
+
+
 for f in glob.glob('Results/MAFFT/*.fasta'):
     aln = list(AlignIO.read(open(f), "fasta"))
     for i in range(1, len(aln)):
@@ -31,8 +31,8 @@ for f in glob.glob('Results/MAFFT/*.fasta'):
         for j in range(len(aln[i].seq)):
             if aln[i].seq[j].upper() != aln[0].seq[j].upper():
                 print("  position : " + str(j) + " reference base: " + aln[0].seq[j] + " sequence base: " + aln[i].seq[j], file=open("mafft-output.txt", "a")) 
-"""
 
+"""
 # horizontal alignment
 haln = list(AlignIO.read("Results/Horizontal/aln-kalign-horizontal.fasta", "fasta"))
 for i in range(1, len(haln)):
@@ -40,3 +40,4 @@ for i in range(1, len(haln)):
     for j in range(len(haln[i].seq)):
         if haln[i].seq[j].upper() != haln[0].seq[j].upper():
             print("  position : " + str(j) + " reference base: " + haln[0].seq[j] + " sequence base: " + haln[i].seq[j], file=open("kalign-horizontal-output.txt", "a")) 
+"""
